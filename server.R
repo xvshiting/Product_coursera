c547640051e20ece5bf53db0d5f84e07d513be31
xvshiting <597241336@qq.com>
2015-09-27 01:10:37 +0800

    first answer

---
+++ server.R
@@ -0,0 +1,13 @@
+library(datasets);
+library(ggplot2)
+data("mtcars")
+fit<-lm(mpg~am+hp+wt,data = mtcars)
+shinyServer(
+
+  function(input, output) {
+    
+    output$distPlot<-renderPrint(34.00287512*as.numeric({input$dist})+2.08371013*(1-as.numeric({input$dist}))-0.03747873*as.numeric({input$caption})-2.87857541*as.numeric({input$obs}))
+    g<-ggplot(data = mtcars,aes(wt,mpg))+geom_point()
+    output$pl<-renderPlot(g)
+  }
+)
---
+++ wwww/ui.R
@@ -0,0 +1,39 @@
+library(datasets)
+data("mtcars")
+fit<-lm(mpg~am+hp+wt,data = mtcars)
+shinyUI(pageWithSidebar(
+  headerPanel("Predict your car's MPG"),
+  
+  sidebarPanel(
+    sliderInput("obs",
+                "weight of cars:",
+                min = 1,
+                max = 6,
+                value=2,step = 0.01),
+    
+    radioButtons("dist", "Transmission type:",
+                 c("Manual" = 1,
+                   "Automatic" = 0
+                 )),
+    
+    numericInput("caption", "HorsePower:",100),
+    
+    submitButton("Calculate")
+    
+  ),
+  
+  
+  mainPanel(
+    verbatimTextOutput("distPlot"),
+    plotOutput("pl",plot(mtcars$mpg,mtcars$wt),width = "400px",height = "400px"),
+    helpText("This application used to calculate the miles per gallon of your car ,you could input three value"),
+    helpText("1.Weight of your car"),
+    helpText("2.Your cars Transmission type"),
+    helpText("3.Horse power of your car\n"),
+    helpText("After you Input all value,you could click the \"calculate\" button"),
+    helpText("Then the result will appear on the right top coner"),
+    helpText("The picture above show a relationship between wt and mpg")
+    
+  )
+)
+)
